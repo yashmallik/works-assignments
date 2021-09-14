6f437623b4d5471d59fecb64f037ca0af765db0b
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-09-14 20:30:34 +0530

    Create Area of circle.c

---
+++ C/Area of circle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  float radius, area;
+
+  printf("Enter the radius of a circle\n");
+
+  scanf("%f", &radius);
+
+  area = 3.14159*radius*radius;
+
+  printf("Area of the circle = %.1f\n", area); 
+  
+
+  return 0;
+}
